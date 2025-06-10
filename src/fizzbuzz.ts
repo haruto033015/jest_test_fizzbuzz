af12ebd6148a9ef4cf4094a70b125aa7fc267582
高橋遼大 <haruto.takahashi@blcloudprj101.sakura.ne.jp>
2025-06-10 13:15:27 +0900

    fizzbuzz success

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,2 +1,15 @@
-export const fizzbuzz = (n: number): string => {
+export const fizzbuzz = (n: number) :string=> {
+    if( n%15 == 0 ){
+        return "FizzBuzz";
+    }else if( n%3 == 0 ){
+        return "Fizz";
+    }else if( n%5 == 0 ){
+        return "Buzz";
+    }
+    return String(n);
 };
+
+// fizzbuzz(1);
+// fizzbuzz(3);
+// fizzbuzz(10);
+// fizzbuzz(15);
